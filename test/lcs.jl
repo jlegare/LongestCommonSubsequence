7cd580894b9dacd0c7944220e38cf9e99331b686
Jacques Légaré <jacques.legare70@gmail.com>
2019-02-03 18:31:53 -0500

    Adding a few sanity tests.

---
+++ test/lcs.jl
@@ -0,0 +1,9 @@
+@testset "Sanity" begin
+    @test lcs([ ], [ ], ==) == ( [ ], [ ] )
+
+    @test lcs([ "a" ], [ ], ==) == ( [ ( true, 1:1:1 ) ], [ ] )
+    @test lcs([ ], [ "a" ], ==) == ( [ ], [ ( true, 1:1:1 ) ] )
+
+    @test lcs([ "a" ], [ "a" ], ==) == ( [ ( false, 1:1:1 ) ], [ ( false, 1:1:1 ) ] )
+    @test lcs([ "a" ], [ "b" ], ==) == ( [ ( true, 1:1:1 ) ], [ ( true, 1:1:1 ) ] )
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using LongestCommonSubsequence
 using Test
 
-tests = [ ]
+tests = [ "lcs", ]
 
 for test ∈ tests
     include("$test.jl")
