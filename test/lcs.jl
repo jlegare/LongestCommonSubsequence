19f39c06577ab33dd180e2a9a71e1a38d1aefdbf
Jacques Légaré <jacques.legare70@gmail.com>
2019-02-03 18:34:30 -0500

    Handle string tests first.

--- test/lcs.jl
+++ test/lcs.jl
@@ -1,9 +1,9 @@
-@testset "Sanity" begin
-    @test lcs([ ], [ ], ==) == ( [ ], [ ] )
+@testset "Sanity/String" begin
+    @test lcs("", "", ==) == ( [ ], [ ] )
 
-    @test lcs([ "a" ], [ ], ==) == ( [ ( true, 1:1:1 ) ], [ ] )
-    @test lcs([ ], [ "a" ], ==) == ( [ ], [ ( true, 1:1:1 ) ] )
+    @test lcs("a", "", ==) == ( [ ( true, 1:1:1 ) ], [ ] )
+    @test lcs("", "a", ==) == ( [ ], [ ( true, 1:1:1 ) ] )
 
-    @test lcs([ "a" ], [ "a" ], ==) == ( [ ( false, 1:1:1 ) ], [ ( false, 1:1:1 ) ] )
-    @test lcs([ "a" ], [ "b" ], ==) == ( [ ( true, 1:1:1 ) ], [ ( true, 1:1:1 ) ] )
+    @test lcs("a", "a", ==) == ( [ ( false, 1:1:1 ) ], [ ( false, 1:1:1 ) ] )
+    @test lcs("a", "b", ==) == ( [ ( true, 1:1:1 ) ], [ ( true, 1:1:1 ) ] )
 end
