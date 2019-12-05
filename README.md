bd6942f799c5ab7daa2f2a8104ce9c3376cc1319
Jacques Légaré <jacques.legare70@gmail.com>
2019-12-05 16:16:17 -0500

    Test strings longer than one character!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## LongestCommonSubsequence
 
 [![Build Status](https://travis-ci.com/jlegare/LongestCommonSubsequence.svg?branch=master)](https://travis-ci.com/jlegare/LongestCommonSubsequence)
-[![Coverage Status](https://coveralls.io/repos/github/jlegare/LongestCommonSubsequence/badge.svg?branch=master)](https://coveralls.io/github/jlegare/LongestCommonSubsequence?branch=master)
+[![Coverage Status](https://coveralls.io/repos/github/jlegare/LongestCommonSubsequence/badge.svg?branch=master&kill_cache=1)](https://coveralls.io/github/jlegare/LongestCommonSubsequence?branch=master)
--- test/lcs.jl
+++ test/lcs.jl
@@ -4,6 +4,17 @@
     @test lcs("a", "", ==) == ( [ ( true, 1:1:1 ) ], [ ] )
     @test lcs("", "a", ==) == ( [ ], [ ( true, 1:1:1 ) ] )
 
+    @test lcs("ab", "", ==) == ( [ ( true, 1:1:2 ) ], [ ] )
+    @test lcs("abc", "", ==) == ( [ ( true, 1:1:3 ) ], [ ] )
+
     @test lcs("a", "a", ==) == ( [ ( false, 1:1:1 ) ], [ ( false, 1:1:1 ) ] )
     @test lcs("a", "b", ==) == ( [ ( true, 1:1:1 ) ], [ ( true, 1:1:1 ) ] )
+
+    @test lcs("a", "ab", ==) == ( [ ( false, 1:1:1 ) ], [ ( false, 1:1:1 ), ( true, 2:1:2 ) ] )
+    @test lcs("a", "bb", ==) == ( [ ( true, 1:1:1 ) ], [ ( true, 1:1:2 ) ] )
+
+    @test lcs("ab", "bb", ==) == ( [ ( true, 1:1:1 ), ( false, 2:1:2 ) ], [ ( true, 1:1:1 ), ( false, 2:1:2 ) ] )
+
+    @test lcs("abc", "bbc", ==) == ( [ ( true, 1:1:1 ), ( false, 2:1:3 ) ], [ ( true, 1:1:1 ), ( false, 2:1:3 ) ] )
+    @test lcs("abc", "abb", ==) == ( [ ( false, 1:1:2 ), ( true, 3:1:3 ) ], [ ( false, 1:1:2 ), ( true, 3:1:3 ) ] )
 end
